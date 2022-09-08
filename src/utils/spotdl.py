3b8e7af4e42907c96390563f6cc4e6e9ec9da523
CoderCatA5 <theayushsingh72@gmail.com>
2022-09-08 09:50:48 +0530

    basic downloader syntax

--- src/utils/spotdl.py
+++ src/utils/spotdl.py
@@ -1,11 +1,11 @@
 import os
 class SpotDL_Downloader():
-    def __init__(self,path):
-        self.path=path
+    def __init__(self,genre):
+        self.genre=genre
     def download(self,uid):    
         path=self.path
-        os.system(f'cd {path}')        
+        os.system(f'cd /abad/{genre}')        
         os.system(f'spotdl download https://open.spotify.com/track/{uid}')
-    
-
+        os.system('cd ..')
+        os.system('cd ..')
         
