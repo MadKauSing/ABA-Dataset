85fbd9f55f604b95aff68d23a117792aebd5f303
CoderCatA5 <theayushsingh72@gmail.com>
2022-10-16 18:56:36 +0530

    back baby

---
+++ install_linux.sh
@@ -0,0 +1,8 @@
+sudo apt install ffmpeg
+pip install virtualenv
+python3 -m venv aba
+
+source aba/bin/activate 
+pip list
+
+pip install -r requirements.txt
---
+++ test.py
@@ -0,0 +1,3 @@
+import os
+var=os.system("cd aba/carnatic && spotdl \"Mahathi - Aasai Mugam\" --output-format mp3" )
+print(var)
